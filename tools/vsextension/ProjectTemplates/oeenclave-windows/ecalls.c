05e6a92300acca99d48f6b2f360dee0454f93137
Dave Thaler <dthaler@microsoft.com>
2020-03-20 14:35:39 -0700

    Use OE_SET_ENCLAVE_{SGX,OPTEE} macros
    The .conf files are kept so that the developer can see how they can be
    optionally used, via comments in those files.
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- tools/vsextension/ProjectTemplates/oeenclave-windows/ecalls.c
+++ tools/vsextension/ProjectTemplates/oeenclave-windows/ecalls.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include "$projectname$_t.h"
 
+#define HEAP_SIZE_BYTES (2 * 1024 * 1024) /* 2 MB */
+#define STACK_SIZE_BYTES (24 * 1024)      /* 24 KB */
+
+#define TA_UUID /* $guid1$ */ \
+    $guid1struct$
+
+OE_SET_ENCLAVE_OPTEE(
+    TA_UUID,                                  /* UUID */
+    HEAP_SIZE_BYTES,                          /* HEAP_SIZE */
+    STACK_SIZE_BYTES,                         /* STACK_SIZE */
+    TA_FLAG_MULTI_SESSION | TA_FLAG_EXEC_DDR, /* FLAGS */
+    "1.0.0",                                  /* VERSION */
+    "$projectname$ TA");                      /* DESCRIPTION */
+
+OE_SET_ENCLAVE_SGX(
+    1,                                        /* ProductID */
+    1,                                        /* SecurityVersion */
+#ifdef _DEBUG
+    1,                                        /* Debug */
+#else
+    0,                                        /* Debug */
+#endif
+    HEAP_SIZE_BYTES / 4096,                   /* NumHeapPages */
+    STACK_SIZE_BYTES / 4096,                  /* NumStackPages */
+    1);                                       /* NumTCS */
+
 int ecall_DoWorkInEnclave(void)
 {
     /* Implement your ECALL here. */
