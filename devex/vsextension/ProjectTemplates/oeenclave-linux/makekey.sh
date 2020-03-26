614d0f4563ee07bc41052dcecbf133e9468efb8a
Dave Thaler <dthaler@microsoft.com>
2020-03-25 17:42:20 -0700

    Fix check-ci complaint about -o in a shell script
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- devex/vsextension/ProjectTemplates/oeenclave-linux/makekey.sh
+++ devex/vsextension/ProjectTemplates/oeenclave-linux/makekey.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 if [ ! -f private.pem ]; then openssl genrsa -out private.pem -3 3072; fi
-if [ ! -f public.pem -o private.pem -nt public.pem ]; then openssl rsa -in private.pem -pubout -out public.pem; fi
+if [ ! -f public.pem ] || [ private.pem -nt public.pem ]; then openssl rsa -in private.pem -pubout -out public.pem; fi
